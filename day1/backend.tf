c0676c908821740ad39f04ce5b2d81055623e0b5
Akhilesh Mishra <akhileshmishrabiz@gmail.com>
2024-05-11 18:31:02 +0530

    terraform day1 - remote backend

--- day1/backend.tf
+++ day1/backend.tf
@@ -1,5 +1,14 @@
+# terraform {
+#   backend "local" {
+#     path = "terraform.tfstate"
+#   }
+# }
+
+
 terraform {
-  backend "local" {
-    path = "terraform.tfstate"
+  backend "s3" {
+    bucket = "366140438193-terraform-state"
+    key    = "terraform101/akhilesh/terraform.tfstate"
+    region = "ap-south-1"
   }
 }
