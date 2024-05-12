017c521b6aa6cc2e834e2dbcfbe27c55bcb48ae5
Akhilesh Mishra <akhileshmishrabiz@gmail.com>
2024-05-12 17:40:42 +0530

    fix

--- day2/main.tf
+++ day2/main.tf
@@ -1,34 +1,3 @@
-# data -> when you want to refrence exusting resources use data block
-# resource block 
-
-
-resource "aws_vpc" "main" {
-#   cidr_block = "10.0.0.0/16"
-    cidr_block = var.vpc_cidr
-
-    tags = {
-    Name = "main-vpc"
-  }
-} 
-
-resource "aws_internet_gateway" "gw" {
-  vpc_id = aws_vpc.main.id
-
-  tags = {
-    Name = "main"
-  }
-}
-resource "aws_subnet" "mohit" {
-  vpc_id = aws_vpc.main.id   # Implicit dependency 
-  cidr_block = "10.0.1.0/24"
-   map_public_ip_on_launch = true
-  availability_zone = var.availability_zone
-  tags = {
-    Name = "mohitsubnet"
-  }
-
-  depends_on = [ aws_vpc.main ] # explict dependency 
-}
 
 resource "aws_instance" "web" {
   ami           =var.ami
